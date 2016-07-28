5ce9d9359b27a99681a72cf0d33e6819c62ea62d
Brandon Adams <emidln@gmail.com>
2016-07-28 11:20:07 -0500

    Fake the deploy for lein release while I figure out clojars

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :main com.emidln.migrations
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :lein-release {:deploy-via :shell
+                 :shell ["echo" "\"fake deploy\""]})
